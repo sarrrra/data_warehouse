fe12812537b8e134f7da151b1d3cc1e4596dcd0a
sara <49569310+sarrrra@users.noreply.github.com>
2025-12-26 11:54:53 +0100

    Update load_bronze.sql

--- scripts/bronze/load_bronze.sql
+++ scripts/bronze/load_bronze.sql
@@ -38,7 +38,7 @@ BEGIN
     TRUNCATE TABLE bronze.patient;
     
     RAISE NOTICE '>> Inserting Data Into: bronze.patient';
-    COPY bronze.patient FROM 'C:\DataProjects\MyBIProject\data_warehouse\patient.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+    COPY bronze.patient FROM 'C:\DataProjects\MyBIProject\data_warehouse\patient.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL '');
     
     v_end_time := clock_timestamp();
     RAISE NOTICE '>> Load Duration: % seconds', ROUND(EXTRACT(EPOCH FROM (v_end_time - v_start_time))::numeric, 2);
@@ -52,7 +52,7 @@ BEGIN
     TRUNCATE TABLE bronze.diagnostic;
     
     RAISE NOTICE '>> Inserting Data Into: bronze.diagnostic';
-    COPY bronze.diagnostic FROM 'C:\DataProjects\MyBIProject\data_warehouse\diagnostic.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+    COPY bronze.diagnostic FROM 'C:\DataProjects\MyBIProject\data_warehouse\diagnostic.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL '');
     
     v_end_time := clock_timestamp();
     RAISE NOTICE '>> Load Duration: % seconds', ROUND(EXTRACT(EPOCH FROM (v_end_time - v_start_time))::numeric, 2);
@@ -66,7 +66,7 @@ BEGIN
     TRUNCATE TABLE bronze.professional;
     
     RAISE NOTICE '>> Inserting Data Into: bronze.professional';
-    COPY bronze.professional FROM 'C:\DataProjects\MyBIProject\data_warehouse\professional.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+    COPY bronze.professional FROM 'C:\DataProjects\MyBIProject\data_warehouse\professional.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL '');
     
     v_end_time := clock_timestamp();
     RAISE NOTICE '>> Load Duration: % seconds', ROUND(EXTRACT(EPOCH FROM (v_end_time - v_start_time))::numeric, 2);
@@ -80,7 +80,7 @@ BEGIN
     TRUNCATE TABLE bronze.speciality;
     
     RAISE NOTICE '>> Inserting Data Into: bronze.speciality';
-    COPY bronze.speciality FROM 'C:\DataProjects\MyBIProject\data_warehouse\speciality.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+    COPY bronze.speciality FROM 'C:\DataProjects\MyBIProject\data_warehouse\speciality.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL '');
     
     v_end_time := clock_timestamp();
     RAISE NOTICE '>> Load Duration: % seconds', ROUND(EXTRACT(EPOCH FROM (v_end_time - v_start_time))::numeric, 2);
@@ -94,7 +94,7 @@ BEGIN
     TRUNCATE TABLE bronze.consultation;
     
     RAISE NOTICE '>> Inserting Data Into: bronze.consultation';
-    COPY bronze.consultation FROM 'C:\DataProjects\MyBIProject\data_warehouse\consultation.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+    COPY bronze.consultation FROM 'C:\DataProjects\MyBIProject\data_warehouse\consultation.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL '');
     
     v_end_time := clock_timestamp();
     RAISE NOTICE '>> Load Duration: % seconds', ROUND(EXTRACT(EPOCH FROM (v_end_time - v_start_time))::numeric, 2);
@@ -108,7 +108,7 @@ BEGIN
     TRUNCATE TABLE bronze.medication;
     
     RAISE NOTICE '>> Inserting Data Into: bronze.medication';
-    COPY bronze.medication FROM 'C:\DataProjects\MyBIProject\data_warehouse\medication.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+    COPY bronze.medication FROM 'C:\DataProjects\MyBIProject\data_warehouse\medication.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL '');
     
     v_end_time := clock_timestamp();
     RAISE NOTICE '>> Load Duration: % seconds', ROUND(EXTRACT(EPOCH FROM (v_end_time - v_start_time))::numeric, 2);
@@ -122,7 +122,7 @@ BEGIN
     TRUNCATE TABLE bronze.laboratory;
     
     RAISE NOTICE '>> Inserting Data Into: bronze.laboratory';
-    COPY bronze.laboratory FROM 'C:\DataProjects\MyBIProject\data_warehouse\laboratory.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+    COPY bronze.laboratory FROM 'C:\DataProjects\MyBIProject\data_warehouse\laboratory.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL '');
     
     v_end_time := clock_timestamp();
     RAISE NOTICE '>> Load Duration: % seconds', ROUND(EXTRACT(EPOCH FROM (v_end_time - v_start_time))::numeric, 2);
@@ -136,7 +136,7 @@ BEGIN
     TRUNCATE TABLE bronze.mutuelle;
     
     RAISE NOTICE '>> Inserting Data Into: bronze.mutuelle';
-    COPY bronze.mutuelle FROM 'C:\DataProjects\MyBIProject\data_warehouse\mutuelle.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+    COPY bronze.mutuelle FROM 'C:\DataProjects\MyBIProject\data_warehouse\mutuelle.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL '');
     
     v_end_time := clock_timestamp();
     RAISE NOTICE '>> Load Duration: % seconds', ROUND(EXTRACT(EPOCH FROM (v_end_time - v_start_time))::numeric, 2);
@@ -150,7 +150,7 @@ BEGIN
     TRUNCATE TABLE bronze.prescription;
     
     RAISE NOTICE '>> Inserting Data Into: bronze.prescription';
-    COPY bronze.prescription FROM 'C:\DataProjects\MyBIProject\data_warehouse\prescription.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+    COPY bronze.prescription FROM 'C:\DataProjects\MyBIProject\data_warehouse\prescription.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL '');
     
     v_end_time := clock_timestamp();
     RAISE NOTICE '>> Load Duration: % seconds', ROUND(EXTRACT(EPOCH FROM (v_end_time - v_start_time))::numeric, 2);
@@ -164,7 +164,7 @@ BEGIN
     TRUNCATE TABLE bronze.room;
     
     RAISE NOTICE '>> Inserting Data Into: bronze.room';
-    COPY bronze.room FROM 'C:\DataProjects\MyBIProject\data_warehouse\room.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+    COPY bronze.room FROM 'C:\DataProjects\MyBIProject\data_warehouse\room.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL '');
     
     v_end_time := clock_timestamp();
     RAISE NOTICE '>> Load Duration: % seconds', ROUND(EXTRACT(EPOCH FROM (v_end_time - v_start_time))::numeric, 2);
@@ -178,7 +178,109 @@ BEGIN
     TRUNCATE TABLE bronze.date_lookup;
     
     RAISE NOTICE '>> Inserting Data Into: bronze.date_lookup';
-    COPY bronze.date_lookup FROM 'C:\DataProjects\MyBIProject\data_warehouse\date.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+    COPY bronze.date_lookup FROM 'C:\DataProjects\MyBIProject\data_warehouse\date.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL '');
+    
+    v_end_time := clock_timestamp();
+    RAISE NOTICE '>> Load Duration: % seconds', ROUND(EXTRACT(EPOCH FROM (v_end_time - v_start_time))::numeric, 2);
+    RAISE NOTICE '>> -------------';
+
+	--------------------------------------------------------
+    -- 12. Death Data
+    --------------------------------------------------------
+    v_start_time := clock_timestamp();
+    RAISE NOTICE '>> Truncating Table: bronze.death';
+    TRUNCATE TABLE bronze.death;
+    
+    RAISE NOTICE '>> Inserting Data Into: bronze.death';
+    COPY bronze.death FROM 'C:\DataProjects\MyBIProject\data_warehouse\DECES EN FRANCE\deces.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL '');
+    
+    v_end_time := clock_timestamp();
+    RAISE NOTICE '>> Load Duration: % seconds', ROUND(EXTRACT(EPOCH FROM (v_end_time - v_start_time))::numeric, 2);
+    RAISE NOTICE '>> -------------';
+
+    --------------------------------------------------------
+    -- 13. Professional Activity (Semicolon Fix)
+    --------------------------------------------------------
+    v_start_time := clock_timestamp();
+    RAISE NOTICE '>> Truncating Table: bronze.professional_activity';
+    TRUNCATE TABLE bronze.professional_activity;
+    
+    RAISE NOTICE '>> Inserting Data Into: bronze.professional_activity';
+    
+    -- Changed DELIMITER to ';'
+    -- Added ENCODING 'latin1' for French characters
+    -- Added NULL '' for the empty cases
+    COPY bronze.professional_activity 
+    FROM 'C:\DataProjects\MyBIProject\data_warehouse\Etablissement de SANTE\activite_professionnel_sante.csv' 
+    WITH (
+        FORMAT csv, 
+        HEADER true, 
+        DELIMITER ';', 
+        NULL '',
+        ENCODING 'latin1'
+    );
+    
+    v_end_time := clock_timestamp();
+    RAISE NOTICE '>> Load Duration: % seconds', ROUND(EXTRACT(EPOCH FROM (v_end_time - v_start_time))::numeric, 2);
+    RAISE NOTICE '>> -------------';
+    --------------------------------------------------------
+    -- 14. Healthcare Facility
+    --------------------------------------------------------
+    v_start_time := clock_timestamp();
+    RAISE NOTICE '>> Truncating Table: bronze.healthcare_facility';
+    TRUNCATE TABLE bronze.healthcare_facility;
+    
+    RAISE NOTICE '>> Inserting Data Into: bronze.healthcare_facility';
+    COPY bronze.healthcare_facility FROM 'C:\DataProjects\MyBIProject\data_warehouse\Etablissement de SANTE\etablissement_sante.csv' 
+	WITH (
+        FORMAT csv, 
+        HEADER true, 
+        DELIMITER ';', 
+        NULL '',
+        ENCODING 'latin1'
+    );
+    
+    v_end_time := clock_timestamp();
+    RAISE NOTICE '>> Load Duration: % seconds', ROUND(EXTRACT(EPOCH FROM (v_end_time - v_start_time))::numeric, 2);
+    RAISE NOTICE '>> -------------';
+
+    --------------------------------------------------------
+    -- 15. Professional Details
+    --------------------------------------------------------
+    v_start_time := clock_timestamp();
+    RAISE NOTICE '>> Truncating Table: bronze.professional_details';
+    TRUNCATE TABLE bronze.professional_details;
+    
+    RAISE NOTICE '>> Inserting Data Into: bronze.professional_details';
+    COPY bronze.professional_details FROM 'C:\DataProjects\MyBIProject\data_warehouse\Etablissement de SANTE\professionnel_sante.csv' 
+	WITH (
+        FORMAT csv, 
+        HEADER true, 
+        DELIMITER ';', 
+        NULL '',
+        ENCODING 'latin1'
+    );
+    
+    v_end_time := clock_timestamp();
+    RAISE NOTICE '>> Load Duration: % seconds', ROUND(EXTRACT(EPOCH FROM (v_end_time - v_start_time))::numeric, 2);
+    RAISE NOTICE '>> -------------';
+
+    --------------------------------------------------------
+    -- 16. Hospitalization
+    --------------------------------------------------------
+    v_start_time := clock_timestamp();
+    RAISE NOTICE '>> Truncating Table: bronze.hospitalization';
+    TRUNCATE TABLE bronze.hospitalization;
+    
+    RAISE NOTICE '>> Inserting Data Into: bronze.hospitalization';
+    COPY bronze.hospitalization FROM 'C:\DataProjects\MyBIProject\data_warehouse\Hospitalisation\Hospitalisations 02-03-2023.csv'
+	WITH (
+        FORMAT csv, 
+        HEADER true, 
+        DELIMITER ';', 
+        NULL '',
+        ENCODING 'latin1'
+    );
     
     v_end_time := clock_timestamp();
     RAISE NOTICE '>> Load Duration: % seconds', ROUND(EXTRACT(EPOCH FROM (v_end_time - v_start_time))::numeric, 2);
@@ -199,3 +301,5 @@ EXCEPTION WHEN OTHERS THEN
     RAISE NOTICE '==========================================';
 END;
 $$;
+
+
